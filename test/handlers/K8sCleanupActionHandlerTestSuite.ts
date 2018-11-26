830dce7930392b8426ee605c5ab859575e2a315a
Igor Ognichenko <igor.ognichenko@fireblink.com>
2018-11-26 15:16:21 +0200

    implemend tests for sc and pvc

--- test/handlers/K8sCleanupActionHandlerTestSuite.ts
+++ test/handlers/K8sCleanupActionHandlerTestSuite.ts
@@ -62,90 +62,169 @@ export class K8sCleanupActionHandlerTestSuite {
 
     @test()
     async cleanupConfigMap() {
-        await this.cleanupK8sObject('ConfigMap');
+        await this.cleanupConfigMapAndSecret('ConfigMap');
     }
 
     @test()
     async cleanupSecret() {
-        await this.cleanupK8sObject('Secret');
+        await this.cleanupConfigMapAndSecret('Secret');
     }
 
-    async cleanupPersistentVolumeClaim() {
-        // TODO: implement
-    }
-
+    @test()
     async cleanupStorageClass() {
-        // TODO: implement
+        await this.applyTestObjects(
+            {
+                kind: 'StorageClass',
+                apiVersion: 'storage.k8s.io/v1',
+                metadata: {
+                    name: 'config-deployed'
+                },
+                provisioner: 'kubernetes.io/aws-ebs',
+                parameters: {
+                    type: 'gp2'
+                },
+                volumeBindingMode: 'Immediate'
+            },
+            {
+                kind: 'StorageClass',
+                apiVersion: 'storage.k8s.io/v1',
+                metadata: {
+                    name: 'config-cluster'
+                },
+                provisioner: 'kubernetes.io/aws-ebs',
+                parameters: {
+                    type: 'gp2'
+                },
+                volumeBindingMode: 'Immediate'
+            },
+            'StorageClass'
+        );
     }
 
-    private async cleanupK8sObject(kind: string) {
-        const startupList = await Container.get(K8sKubectlService).listObjects(kind);
-
-        console.log('------------------------------------------------------------------------------------------------', startupList);
-
-
-        for (const objName of startupList) {
-            await Container.get(K8sKubectlService).deleteObject({
-                kind: kind,
-                apiVersion: 'v1',
-                metadata: {
-                    name: objName
-                }
-            });
-        }
-
+    @test()
+    async cleanupPersistentVolumeClaim() {
         const applyK8sObjectActionHandler = new K8sApplyObjectActionHandler();
         const context = ContextUtil.generateEmptyContext();
         const snapshot = new ActionSnapshot('.', {}, '', 0, {});
 
-        const deployedConfigMapOptions = {
-            kind: kind,
-            apiVersion: 'v1',
-            data: {
-                test: 'true'
-            },
+        const k8sObject = {
+            kind: 'StorageClass',
+            apiVersion: 'storage.k8s.io/v1',
             metadata: {
-                name: 'config-deployed'
-            }
+                name: 'test-storage-class'
+            },
+            provisioner: 'kubernetes.io/aws-ebs',
+            parameters: {
+                type: 'gp2'
+            },
+            volumeBindingMode: 'Immediate'
         };
 
-        const inClusterConfigMapOptions = {
-            kind: kind,
-            apiVersion: 'v1',
-            data: {
-                test: 'true'
+        await applyK8sObjectActionHandler.validate(k8sObject, context, snapshot, {});
+        await applyK8sObjectActionHandler.execute(k8sObject, context, snapshot, {});
+
+        await this.applyTestObjects(
+            {
+                apiVersion: 'v1',
+                kind: 'PersistentVolumeClaim',
+                metadata: {
+                    namespace: 'default',
+                    name: 'config-deployed',
+                },
+                spec: {
+                    volumeMode: 'Filesystem',
+                    resources: {
+                        requests: {
+                            storage: '8Gi'
+                        }
+                    },
+                    accessModes: [
+                        'ReadWriteOnce'
+                    ],
+                    storageClassName: 'test-storage-class'
+                }
             },
-            metadata: {
-                name: 'config-cluster'
-            }
-        };
+            {
+                apiVersion: 'v1',
+                kind: 'PersistentVolumeClaim',
+                metadata: {
+                    namespace: 'default',
+                    name: 'config-cluster',
+                },
+                spec: {
+                    volumeMode: 'Filesystem',
+                    resources: {
+                        requests: {
+                            storage: '8Gi'
+                        }
+                    },
+                    accessModes: [
+                        'ReadWriteOnce'
+                    ],
+                    storageClassName: 'test-storage-class'
+                }
+            },
+            'pvc'
+        );
+    }
 
-        await applyK8sObjectActionHandler.validate(deployedConfigMapOptions, context, snapshot, {});
-        await applyK8sObjectActionHandler.execute(deployedConfigMapOptions, context, snapshot, {});
+    private async applyTestObjects(deployedObject: object, clusterObject: object, kind: string) {
+        const applyK8sObjectActionHandler = new K8sApplyObjectActionHandler();
+        const context = ContextUtil.generateEmptyContext();
+        const snapshot = new ActionSnapshot('.', {}, '', 0, {});
 
-        await applyK8sObjectActionHandler.validate(inClusterConfigMapOptions, context, snapshot, {});
-        await applyK8sObjectActionHandler.execute(inClusterConfigMapOptions, context, snapshot, {});
+        await applyK8sObjectActionHandler.validate(deployedObject, context, snapshot, {});
+        await applyK8sObjectActionHandler.execute(deployedObject, context, snapshot, {});
+
+        await applyK8sObjectActionHandler.validate(clusterObject, context, snapshot, {});
+        await applyK8sObjectActionHandler.execute(clusterObject, context, snapshot, {});
 
         context.entities.registered.splice(-1, 1);
 
         assert.strictEqual(context.entities.registered.length, 1);
 
-        const configMaps = await Container.get(K8sKubectlService).listObjects(kind);
-        assert.strictEqual(configMaps.length, 2);
+        const configMaps = await Container.get(K8sKubectlService).listObjects(kind, 'default');
+
+        chai.expect(configMaps).to.be.an('array').that.includes('config-cluster');
+        chai.expect(configMaps).to.be.an('array').that.includes('config-deployed');
 
         const actionHandler = new K8sCleanupActionHandler();
         const cleanupOptions = {
             namespace: 'default',
-            allowed: {
-                secrets: ['default-token-*']
-            }
         };
 
         await actionHandler.validate(cleanupOptions, context, snapshot, {});
         await actionHandler.execute(cleanupOptions, context, snapshot, {});
 
-        const configMapsAfterCleanup = await Container.get(K8sKubectlService).listObjects(kind);
+        const objectsAfterCleanup = await Container.get(K8sKubectlService).listObjects(kind, 'default');
 
-        assert.strictEqual(configMapsAfterCleanup.length, 1);
+        chai.expect(objectsAfterCleanup).to.be.an('array').that.not.includes('config-cluster');
+        chai.expect(objectsAfterCleanup).to.be.an('array').that.includes('config-deployed');
+    }
+
+    private async cleanupConfigMapAndSecret(kind: string) {
+        await this.applyTestObjects(
+            {
+                kind: kind,
+                apiVersion: 'v1',
+                data: {
+                    test: 'true'
+                },
+                metadata: {
+                    name: 'config-deployed'
+                }
+            },
+            {
+                kind: kind,
+                apiVersion: 'v1',
+                data: {
+                    test: 'true'
+                },
+                metadata: {
+                    name: 'config-cluster'
+                }
+            },
+            kind
+        );
     }
 }
