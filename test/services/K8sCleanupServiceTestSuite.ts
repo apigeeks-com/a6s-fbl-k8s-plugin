7aa56a69c0d513f1275cd2e3f475bac8925ce82b
Vitalii Trachenko <vitaltrachenko@gmail.com>
2018-12-21 11:33:14 +0200

    test(cleanup service): 100% coverage of K8sCleanupService
    fix #32

--- test/services/K8sCleanupServiceTestSuite.ts
+++ test/services/K8sCleanupServiceTestSuite.ts
@@ -1,5 +1,5 @@
-import { K8sCleanupService } from '../../src/services';
-import { IK8sCleanupOptions } from '../../src/interfaces';
+import { K8sCleanupService, K8sHelmService, K8sKubectlService } from '../../src/services';
+import { IK8sCleanupOptions, IK8sObject } from '../../src/interfaces';
 import { ContextUtil, ActionSnapshot, FlowService, IActionStep, IContext } from 'fbl';
 import { suite, test } from 'mocha-typescript';
 import { Container } from 'typedi';
@@ -9,6 +9,8 @@ import * as assert from 'assert';
 export class K8sCleanupServiceTestSuite extends K8sCleanupService {
     constructor() {
         super();
+        this.k8sHelmService = Container.get(K8sHelmService);
+        this.k8sKubectlService = Container.get(K8sKubectlService);
     }
 
     cleanUpHelms(
@@ -21,8 +23,30 @@ export class K8sCleanupServiceTestSuite extends K8sCleanupService {
         return super.cleanUpHelmReleases(options, context, snapshot, registeredHelmReleases, deployedHelms);
     }
 
+    cleanUpK8sObjects(
+        options: IK8sCleanupOptions,
+        context: IContext,
+        snapshot: ActionSnapshot,
+        kind: string,
+        allHelmObjects: IK8sObject[],
+        deployed: string[],
+        cluster: string[],
+        ignoredPatterns: string[],
+    ): Promise<void> {
+        return super.cleanupK8sObjects(
+            options,
+            context,
+            snapshot,
+            kind,
+            allHelmObjects,
+            deployed,
+            cluster,
+            ignoredPatterns,
+        );
+    }
+
     @test()
-    async checkCleanHelmsFailed() {
+    async failCleanupHelms() {
         Container.get(FlowService).debug = true;
 
         const context = ContextUtil.generateEmptyContext();
@@ -43,5 +67,37 @@ export class K8sCleanupServiceTestSuite extends K8sCleanupService {
             .map(log => log.payload);
 
         assert(logs[0].indexOf(`Helm release "${helmName}" failed to delete:`) >= 0);
+    }
+
+    @test()
+    async failCleanupK8sObjects() {
+        Container.get(FlowService).debug = true;
+
+        const context = ContextUtil.generateEmptyContext();
+        const snapshot = new ActionSnapshot('.', {}, '', 0, {});
+        const clusterMember = 'not-existed-cluster-source';
+        const cleanupOptions = {
+            dryRun: false,
+            namespace: 'default',
+            kind: 'ConfigMap',
+        };
+
+        await this.cleanUpK8sObjects(
+            cleanupOptions,
+            context,
+            snapshot,
+            cleanupOptions.kind,
+            [],
+            [],
+            [clusterMember],
+            [],
+        );
+
+        const logs: string[] = await snapshot
+            .getSteps()
+            .filter((step: IActionStep) => step.type === 'log')
+            .map(log => log.payload);
+
+        assert(logs[0].indexOf(`${cleanupOptions.kind} "${clusterMember}" failed to delete:`) >= 0);
     }
 }
